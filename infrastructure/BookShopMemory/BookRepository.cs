b28954542f647e757667faf77dd56f40b6d221bf
Sergey Yusupov <sergey.yusupov@acumatica.com>
2020-07-18 23:54:41 +0300

    added new project

---
+++ infrastructure/BookShopMemory/BookRepository.cs
@@ -0,0 +1,25 @@
+﻿using Bookshop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookShopMemory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Progaramming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language")
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books
+                .Where(book => book.Title.Contains(titlePart))
+                .ToArray();
+        }
+    }
+}
