2b288bf6d6857352152fc8204741e092df6ec2d2
Sergey Yusupov <sergey.yusupov@acumatica.com>
2020-07-20 00:38:21 +0300

    add page of search

---
+++ presentation/BookShopWeb/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookshop;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopWeb.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepository bookRepository;
+
+        public SearchController(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+        public IActionResult Index(string query)
+        {
+            var books = bookRepository.GetAllByTitle(query);
+
+            return View(books);
+        }
+    }
+}
